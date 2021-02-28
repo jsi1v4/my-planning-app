f387ad5116014bdab0e01e02c8fed162f4562b56
Jose Silva <me@jsi1v4.dev>
2021-02-28 14:43:27 -0300

    feat: confirm popover in sign off

--- src/components/app-signoff/index.jsx
+++ src/components/app-signoff/index.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import { Button, Tooltip } from 'antd';
+import { Button, Tooltip, Popconfirm } from 'antd';
 import { LogoutOutlined } from '@ant-design/icons';
 
+import { useI18n } from '../../i18n';
+
 export function Signoff({ userName, signOut }) {
+  const t = useI18n();
+
   return (
     <Tooltip title={userName} placement="bottomRight">
-      <Button
-        type="text"
-        shape="circle"
-        data-testid="sign-out"
-        icon={<LogoutOutlined />}
-        onClick={signOut}
-      />
+      <Popconfirm
+        title={t('app_signoff-confirm')}
+        onConfirm={signOut}
+        okText={t('app_signoff-ok')}
+        cancelText={t('app_signoff-cancel')}
+        placement="bottomRight"
+      >
+        <Button
+          type="text"
+          shape="circle"
+          data-testid="sign-out"
+          icon={<LogoutOutlined />}
+        />
+      </Popconfirm>
     </Tooltip>
   );
 }
