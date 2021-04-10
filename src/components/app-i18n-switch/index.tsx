b9d7b82cfdc9c33c7d26780505a07f2815e6ffe0
Jose Silva <me@jsi1v4.dev>
2021-04-10 16:41:18 -0300

    refactor(i18n): radio comp to menu

--- src/components/app-i18n-switch/index.tsx
+++ src/components/app-i18n-switch/index.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
-import { Radio } from 'antd';
-import { RadioChangeEvent } from 'antd/lib/radio';
+import { Button, Dropdown, Menu } from 'antd';
+import { ClickParam } from 'antd/lib/menu';
+import { GlobalOutlined, TranslationOutlined } from '@ant-design/icons';
 
-import { useI18n } from 'src/i18n';
+import { useI18n, useI18nMessage } from 'src/i18n';
 import { Locale } from 'src/i18n/types';
 
 export function I18nSwitch() {
   const { lang, changeLang } = useI18n();
+  const t = useI18nMessage();
 
-  const handleRadio = (e: RadioChangeEvent) => {
-    changeLang(e.target.value);
+  const nameFormatter = (value?: string) =>
+    String(value).toLowerCase().replace(/_/g, '-');
+
+  const handleMenu = (e: ClickParam) => {
+    changeLang(e.key as Locale);
   };
 
+  const menu = (
+    <Menu theme="dark" selectedKeys={[lang]} onClick={handleMenu}>
+      {Object.entries(Locale).map(([key, value]) => (
+        <Menu.Item key={value} icon={<GlobalOutlined />}>
+          {t(`i18n-${nameFormatter(key)}`)}
+        </Menu.Item>
+      ))}
+    </Menu>
+  );
+
   return (
-    <Radio.Group value={lang} onChange={handleRadio}>
-      <Radio.Button key={0} value={Locale.EN_US}>
-        English
-      </Radio.Button>
-      <Radio.Button key={1} value={Locale.PT_BR}>
-        Português
-      </Radio.Button>
-    </Radio.Group>
+    <Dropdown overlay={menu} placement="bottomRight">
+      <Button
+        type="text"
+        shape="circle"
+        data-testid="i18n"
+        icon={<TranslationOutlined />}
+      />
+    </Dropdown>
   );
 }
 
